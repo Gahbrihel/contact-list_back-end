80c1a8742462481dd7f7f637008a7388ee700d97
Gahbrihel <gscampos03@gmail.com>
2025-07-06 14:16:30 -0300

    tentativa erro cors n4

--- src/main/java/br/gov/sp/fatec/itu/contact/controllers/ContactController.java
+++ src/main/java/br/gov/sp/fatec/itu/contact/controllers/ContactController.java
@@ -1,10 +1,21 @@
 package br.gov.sp.fatec.itu.contact.controllers;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import br.gov.sp.fatec.itu.contact.entities.Contact;
+
 import br.gov.sp.fatec.itu.contact.services.ContactService;
+
 import java.util.List;
 
 @RestController
@@ -14,43 +25,25 @@ public class ContactController {
     @Autowired
     private ContactService service;
     
-    // Adiciona endpoint OPTIONS para cada rota
-    @RequestMapping(method = RequestMethod.OPTIONS)
-    public ResponseEntity<?> handleOptions() {
-        return ResponseEntity.ok()
-                .header("Access-Control-Allow-Origin", "*")
-                .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-                .header("Access-Control-Allow-Headers", "*")
-                .header("Access-Control-Max-Age", "3600")
-                .build();
-    }
-    
     @GetMapping
     public ResponseEntity<List<Contact>> getContacts() {
-        return ResponseEntity.ok()
-                .header("Access-Control-Allow-Origin", "*")
-                .body(service.getContacts());
+        return ResponseEntity.ok(service.getContacts());
     }
 
     @PostMapping
     public ResponseEntity<Contact> saveContacts(@RequestBody Contact contact) {
-        return ResponseEntity.created(null)
-                .header("Access-Control-Allow-Origin", "*")
-                .body(service.saveContact(contact));
+        return ResponseEntity.created(null).body(service.saveContact(contact));
     }
     
     @PutMapping("/{id}")
     public ResponseEntity<Contact> updateContact(@PathVariable Long id, @RequestBody Contact contactDetails) {
-        return ResponseEntity.ok()
-                .header("Access-Control-Allow-Origin", "*")
-                .body(service.updateContact(id, contactDetails));
+        return ResponseEntity.ok(service.updateContact(id, contactDetails));
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteContact(@PathVariable Long id) {
         service.deleteContact(id);
-        return ResponseEntity.noContent()
-                .header("Access-Control-Allow-Origin", "*")
-                .build();
+        return ResponseEntity.noContent().build();
     }
+
 }
