d95de8762f3420b63c8f1472b9180e1e206cf521
Gabriel Campos <gscampos03@gmail.com>
2025-07-06 12:27:30 -0300

    :fix: erro de cors

--- src/main/java/br/gov/sp/fatec/itu/contact/controllers/WebConfig.java
+++ src/main/java/br/gov/sp/fatec/itu/contact/controllers/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Permite CORS para todos os endpoints ("/**")
-            .allowedOrigins("http://localhost:4200") // Permite requisições da sua aplicação Angular
+            .allowedOrigins("https://main.d6o0k6zsfzjj2.amplifyapp.com:*") // Permite requisições da sua aplicação Angular
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Libera os métodos HTTP necessários
             .allowedHeaders("*") // Libera todos os cabeçalhos
             .allowCredentials(true);
