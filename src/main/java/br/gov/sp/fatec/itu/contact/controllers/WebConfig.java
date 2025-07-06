668efe6e2dd4b85e27f1dd1d46f1004929ef2926
Gabriel Campos <gscampos03@gmail.com>
2025-07-06 12:37:25 -0300

    Update WebConfig.java

--- src/main/java/br/gov/sp/fatec/itu/contact/controllers/WebConfig.java
+++ src/main/java/br/gov/sp/fatec/itu/contact/controllers/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Permite CORS para todos os endpoints ("/**")
-            .allowedOrigins("https://main.d6o0k6zsfzjj2.amplifyapp.com:4200") // Permite requisições da sua aplicação Angular
+            .allowedOrigins("*") // Permite requisições da sua aplicação Angular
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Libera os métodos HTTP necessários
             .allowedHeaders("*") // Libera todos os cabeçalhos
             .allowCredentials(true);
